26dfa00cb49d40abd632bb4e5b07be1aaf8519c9
jpmjunior <junior.linhares@yahoo.com.br>
2024-06-11 11:52:26 -0300

    projeto base

---
+++ src/main/java/br/com/siaraapps/mtgdb/MtgdbApplication.java
@@ -0,0 +1,13 @@
+package br.com.siaraapps.mtgdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtgdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/siaraapps/mtgdb/MtgdbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.siaraapps.mtgdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtgdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
